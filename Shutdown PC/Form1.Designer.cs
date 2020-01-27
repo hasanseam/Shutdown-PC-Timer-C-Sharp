c2252f4a01ac54e8170fb9fc0b218b5d13c6b57f
Hasan Seam <hasanseam37@gmail.com>
2020-01-27 02:14:25 +0100

    Added by Hasan

---
+++ Shutdown PC/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Shutdown_PC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 25;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox1.Location = new System.Drawing.Point(87, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 33);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kozuka Gothic Pro B", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Kozuka Gothic Pro B", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(181, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minutes";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60"});
+            this.comboBox2.Location = new System.Drawing.Point(270, 44);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(102, 33);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(313, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "hasan.seam";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Kozuka Gothic Pro B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(186, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Shut Down";
+            this.toolTip2.SetToolTip(this.button1, "Start Your Plan");
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Kozuka Gothic Pro B", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(19, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.toolTip2.SetToolTip(this.button2, "Cancel Your Plan");
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(396, 196);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Shut Down";
+            this.toolTip1.SetToolTip(this, "This application will shut down your PC.\r\nProvide \'Hours\' and \'Miniute\'. \r\nUse Sh" +
+        "utDown button to start a plan.\r\nYou can abort your plan clicking on Cancel\r\nbutt" +
+        "on. \r\n\r\n\r\n");
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
---
+++ Shutdown PC/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Shutdown_PC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Shutdown()
+        {
+
+
+            if (comboBox1.Text != "" && comboBox2.Text != "")
+            {
+                int Hours = Convert.ToInt32(comboBox1.Text);
+                int Minit = Convert.ToInt32(comboBox2.Text);
+                int Second = Hours * 3600 + Minit * 60;
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardInput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.RedirectStandardOutput = true; //If I dont have this as true, cmd flashes open and closes
+                Process p = Process.Start(startInfo);
+                p.StandardInput.WriteLine("shutdown -s -t " + Second);
+                p.StandardInput.Flush();
+                p.StandardInput.Close();
+                string error = "";
+                using (StreamReader streamReader = p.StandardError)
+                {
+                    error += streamReader.ReadToEnd();
+                }
+                p.Close();
+                if (error != "")
+                {
+                    MessageBox.Show("Hey You have already started a paln. If You want to change your plan cancel first then retry");
+                }
+                else
+                {
+                    MessageBox.Show("Successfull!! If you want to abort your plan click cancel button");
+                }
+                comboBox1.ResetText();
+                comboBox2.ResetText();
+            }
+            else
+            {
+                string er = "";
+                if (comboBox1.Text == "")
+                {
+                    er += "Set Hours";
+                }
+                if (comboBox2.Text == "")
+                {
+                    if (er == "")
+                    {
+                        er += "Set Miniutes";
+                    }
+                    else
+                    {
+                        er += " And Miniutes";
+                    }
+                }
+
+                if (er != "")
+                {
+                    MessageBox.Show(er);
+                }
+            }
+        }
+
+        private void Abort()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardOutput = true; //If I dont have this as true, cmd flashes open and closes
+            Process p = Process.Start(startInfo);
+            p.StandardInput.WriteLine("shutdown -a");
+            p.StandardInput.Flush();
+            p.StandardInput.Close();
+            string error = "";
+            using (StreamReader streamReader = p.StandardError)
+            {
+                error += streamReader.ReadToEnd();
+            }
+            p.Close();
+            if (error != "")
+            {
+                MessageBox.Show("Sorry No shutdown was in progress.");
+            }
+            else
+            {
+                MessageBox.Show("Succesfully Aborted!!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Abort();
+        }
+
+    }
+}
